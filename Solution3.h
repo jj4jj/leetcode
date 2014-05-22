dc53bd328f1cbe8861f7bd899b1362df3b4c5180
jj4jj <resc@vip.qq.com>
2014-05-22 14:20:30 +0800

    for s3

--- Solution3.h
+++ Solution3.h
@@ -8,53 +8,69 @@
 
 	class Solution3 {
 		public:
-			struct Line
-			{
-				Point slope;
-				Point p0;	
-				Line(Point s,Point p):slope(s.x,s.y),p0(p.x,p.y){}
-			};
 			int maxPoints(vector<Point> &points) {
-				//find them on a same line
 				if(points.size() <= 2)
 				{
 					return points.size();	
 				}
-				std::vector<std::pair<Line,int> >	mp;
+				//sope is Point
+				std::vector<std::pair<Point,int> >	lineCounter;
 				int maxn = 2;
 				for(int i = 0;i < points.size(); ++i)
 				{
-
-					mp.clear();
+					//past points[i] and slope is in counter
+					lineCounter.clear();
+					int iSlop0 = 0;
+					int	iSlopNot0 = 0;
 					for(int j = i+1; j < points.size(); ++j)
 					{
 						Point pt(points[j].x-points[i].x , points[j].y - points[i].y);
-						std::vector<std::pair<Line,int> >::iterator it = mp.begin();
+						std::vector<std::pair<Point,int> >::iterator it = lineCounter.begin();
 						bool bFind = false;
-						while(it != mp.end())
+						while(it != lineCounter.end() && !(pt.x == 0 && pt.y == 0))
 						{
-							//y = kx+b
-							//y2-y1 = k(x2-x1) = pt.y/pt.x(x2-x1)
-							//y2-kx2 = y1 - kx1 -> y2 - y1 = k(x2 - x1)
-							//y1,x1 -> first
-							//y2,x2 -> points j
-							if(pt.x*(it->first.slope.y) == pt.y*(it->first.slope.x) &&
-							   (points[j].y - it->first.p0.y)*pt.x == (points[j].x - it->first.p0.x)*pt.y )
+							//the line in the counter must pass points[i] and slop is lineCounter.first
+							//if j is in a line (parrall with lineCounter.first) , so the lineCounter.second++; 
+							if(pt.x*(it->first.y) == pt.y*(it->first.x))
 							{
 								it->second++;	
-								if(maxn < it->second)
+								if(iSlopNot0 < it->second)
 								{
-									maxn = it->second;
+									iSlopNot0 = it->second;
+									//cout<<points[i].x<<","<<points[i].y<<"->"<<(it->first).x<<","<<(it->first).y<<"->"<<endl;
 								}
 								bFind = true;
+								break;
 							}
 							it++;	
 						}
 						if(false == bFind)
 						{
-							mp.push_back(std::make_pair(Line(pt,points[i]),2));
+							//the line slop is <i,j> and past i
+							if(pt.x == 0 && pt.y == 0 )
+							{
+								iSlop0++;
+								cout<<"0,0:i="<<i<<"slop0="<<iSlop0<<":"<<points[i].x<<","<<points[i].y<<"->"<<points[j].x<<","<<points[j].y<<endl;	
+							}
+							else
+							{
+								lineCounter.push_back(std::make_pair(pt,2));
+								if(0 == iSlopNot0)
+								{
+									iSlopNot0 = 2;
+								}
+							}
 						}
 					}
+					if(iSlopNot0 == 0)
+					{
+						iSlop0++;	
+					}
+					if(maxn < iSlopNot0 + iSlop0)
+					{
+						maxn = iSlopNot0 + iSlop0;	
+					}
+					
 				}
 				return maxn;
 			}
@@ -63,7 +79,7 @@
 				TokenNizer tn;
 				vector<string> lst = tn.Split(line," ");
 				vector<Point>	points;
-				for(int i = 0; i < lst.size(); i+=2)
+				for(int i = 0; i < lst.size()-1; i+=2)
 				 {
 					Point pt(std::atoi(lst[i].c_str()),std::atoi(lst[i+1].c_str())); 
 					points.push_back(pt);	
